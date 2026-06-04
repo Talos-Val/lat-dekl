457c0a0a4e6e29393000182b97ebd690eea9f00f
Titus Wagner <wagner-titus@web.de>
2026-06-04 06:45:36 +0200

    Add files via upload

---
+++ server 3.js
@@ -0,0 +1,20 @@
+const { PeerServer } = require('peer');
+
+const port = process.env.PORT || 9000;
+
+const server = PeerServer({
+  port: port,
+  path: '/peerjs',
+  key: 'peerjs',
+  allow_discovery: false,
+});
+
+server.on('connection', (client) => {
+  console.log('Client verbunden:', client.getId());
+});
+
+server.on('disconnect', (client) => {
+  console.log('Client getrennt:', client.getId());
+});
+
+console.log(`PeerServer läuft auf Port ${port}`);
